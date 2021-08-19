2e73402aad78a4e8245ec826d654e8af3ee993fd
LucíaSaederup <80077276+LuciaSaederup@users.noreply.github.com>
2021-08-19 18:10:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,217 @@
+/////////////////////BOTONES HEADER////////////////
+const header = document.getElementById("header");
+//boton imagen
+const btnPanelImagen = document.getElementById("boton-imagen");
+//boton texto
+const btnPanelTexto = document.getElementById("boton-texto");
+//boton modo oscuro
+const btnModoOscuro = document.getElementById("boton-modo-oscuro");
+const abrirPanel = document.querySelectorAll("abrir-panel");
+
+/////////////////////CUERPO MEME////////////////
+const main = document.getElementById("main");
+//contenedor meme
+const containMeme = document.querySelector(".contain-meme");
+//texto superior
+const topText = document.getElementById("top-text");
+//texto inferior
+const bottomText = document.getElementById("bottom-text");
+//array textos (sup e inf)
+const textosMeme = document.querySelectorAll(".text");
+//box donde va la imagen
+const imgMeme = document.querySelector(".imagen-meme");
+
+/////////////////////BOTON DESCARGA MEME////////////////
+const btnDescargaMeme = document.getElementById("boton-descarga");
+
+/////////////////////ASIDE////////////////
+const aside = document.getElementById("aside");
+
+/////////////////////ASIDE IMAGE OPTIONS////////////////
+//seccion de imagen
+const seccionImg = document.getElementById("section-imagen");
+//input url
+const inputUrlImg = document.getElementById("url-img-options");
+//color fondo
+const inputColorFondoImg = document.getElementById("color-fondo-img");
+const spanColorFondoImg = document.getElementById("span-color-img-fondo");
+//tonalidad fondo
+const selectTonalidadFondoImg = document.getElementById("tonalidad-img-options");
+//filtros
+const filtros = document.querySelectorAll(".input-filtros-img");
+//input brillo
+const brilloFiltrosImg = document.getElementById('brillo-filtros-img');
+//input opacidad
+const opacidadFiltrosImg = document.getElementById('opacidad-filtros-img');
+//input contraste
+const contrasteFiltrosImg = document.getElementById('contraste-filtros-img');
+//input desenfoque
+const desenfoqueFiltrosImg = document.getElementById('desenfoque-filtros-img');
+//input escala de grises
+const escalaFiltrosImg = document.getElementById('escala-filtros-img');
+//input sepia
+const sepiaFiltrosImg = document.getElementById('sepia-filtros-img');
+//input HUE
+const hueFiltrosImg = document.getElementById('hue-filtros-img');
+//input saturado
+const saturadoFiltrosImg = document.getElementById('saturado-filtros-img');
+//input negativo
+const negativoFiltrosImg = document.getElementById('negativo-filtros-img');
+//boton para reestablecer filtros
+const buttonRestablecerFiltrosImg = document.getElementById('button-restablecer-filtros-img');
+
+/////////////////////ASIDE TEXT OPTIONS////////////////
+const seccionTxt = document.getElementById("seccion-texto");
+//texto superior
+const topTextOptions = document.getElementById("top-text-options");
+const btnSinTextoTop = document.getElementById("no-superior-text");
+//texto inferior
+const bottomTextOptions = document.getElementById("bottom-text-options");
+const btnSinTextoBottom = document.getElementById("no-bottom-text");
+//fuente
+const buttonFuentesText = document.getElementById('button-fuentes-text');
+//input tamaño fuente
+const tamañoFuenteTextOptions = document.getElementById('tamaño-fuente-text-options');
+//boton alinear texto a la izquierda
+const buttonLeftTextOptions = document.getElementById('button-left-text-options');
+//boton alinear texto centrado
+const buttonCenterTextOptions = document.getElementById('button-center-text-options');
+//boton alinear texto a la derecha
+const buttonRightTextOptions = document.getElementById('button-right-text-options');
+//input color texto
+const inputColorTexto = document.getElementById('input-color-texto');
+//input color fondo texto
+let inputColorFondoTexto = document.getElementById('input-color-fondo-texto');
+//input fondo transparente
+const transparentTextOption = document.getElementById('transparent-text-option');
+//boton sin contorno el texto
+const buttonNingunoTextOptions = document.getElementById('button-ninguno-text-options');
+//boton contorno texto claro
+const buttonClaroTextOptions = document.getElementById('button-claro-text-options');
+//boton contorno texto oscuro
+const buttonOscuroTextOptions = document.getElementById('button-oscuro-text-options');
+//input espaciado texto
+const espaciadoTextOptions = document.getElementById('espaciado-text-options');
+//input interlineado texto
+const interlineadoTextOptions = document.getElementById('interlineado-text-options');
+
+/////////////////////////////////////FUNCIONALIDADES IMAGEN///////////////////////////////
+//colocar imagen
+inputUrlImg.addEventListener('keyup', () => {
+    const urlImg = inputUrlImg.value;
+    imgMeme.style.backgroundImage = `url('${urlImg}')`;
+});
+//cambiar color imagen
+
+inputColorFondoImg.addEventListener('input', (e) => {
+    const color = e.target.value;
+    imgMeme.style.backgroundColor = `${color}`;
+    spanColorFondoImg.textContent = `${color};`
+});
+
+selectTonalidadFondoImg.addEventListener('change', () => {
+    imgMeme.style.backgroundBlendMode = selectTonalidadFondoImg.value;
+});
+
+//filtros
+
+// Brillo
+brilloFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `brightness(${brilloFiltrosImg.value})`;
+});
+
+// Opacidad
+opacidadFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `opacity(${opacidadFiltrosImg.value})`;
+});
+
+// Contraste
+contrasteFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `contrast(${contrasteFiltrosImg.value}%)`;
+});
+
+//Desenfoque
+desenfoqueFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `blur(${desenfoqueFiltrosImg.value}px)`;
+});
+
+//- Escala de grises
+escalaFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `grayscale(${escalaFiltrosImg.value}%)`;
+});
+
+// Sepia
+sepiaFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `sepia(${sepiaFiltrosImg.value}%)`;
+});
+
+//HUE
+hueFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `hue-rotate(${hueFiltrosImg.value}deg)`;
+});
+
+// Saturado
+saturadoFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `saturate(${saturadoFiltrosImg.value}%)`;
+});
+
+//Negativo
+negativoFiltrosImg.addEventListener('change', () => {
+    imgMeme.style.filter = `invert(${negativoFiltrosImg.value})`;
+});
+
+//Reestablecer valores 
+const filtrosAplicados = () => {
+    imgMeme.style.filter = `opacity(${opacidadFiltrosImg.value})`, imgMeme.style.filter = `opacity(${opacidadFiltrosImg.value})`, imgMeme.style.filter = `contrast(${contrasteFiltrosImg.value})`, imgMeme.style.filter = `blur(${desenfoqueFiltrosImg.value})`, imgMeme.style.filter = `grayscale(${escalaFiltrosImg.value})`, imgMeme.style.filter = `sepia(${sepiaFiltrosImg.value})`, imgMeme.style.filter = `hue-rotate(${hueFiltrosImg.value})`, imgMeme.style.filter = `saturate(${saturadoFiltrosImg.value})`, imgMeme.style.filter = `invert(${negativoFiltrosImg.value})`
+};
+
+buttonRestablecerFiltrosImg.addEventListener('click', () => {
+    brilloFiltrosImg.value = '1';
+    opacidadFiltrosImg.value = '1';
+    contrasteFiltrosImg.value = '100';
+    desenfoqueFiltrosImg.value = '0';
+    escalaFiltrosImg.value = '0';
+    sepiaFiltrosImg.value = '0';
+    hueFiltrosImg.value = '0';
+    saturadoFiltrosImg.value = '100';
+    negativoFiltrosImg.value = '0';
+    filtrosAplicados();
+});
+
+/////////////////////////////////////FUNCIONALIDADES TEXTO///////////////////////////////
+//abrir y cerrar el panel de texto
+btnPanelTexto.addEventListener('click', () => {
+    seccionImg.classList.add('none');
+    seccionTxt.classList.remove('none');
+});
+btnPanelImagen.addEventListener('click', () => {
+    seccionTxt.classList.add('none');
+    seccionImg.classList.remove('none')
+});
+//cambiar texto del meme
+topTextOptions.addEventListener('keyup', (e) => {
+    const textoSuperior = e.target.value;
+    topText.textContent = `${textoSuperior}`;
+});
+bottomTextOptions.addEventListener('keyup', (e) => {
+    const textoInferior = e.target.value;
+    bottomText.textContent = `${textoInferior}`;
+});
+//sacar el texto del meme
+btnSinTextoTop.addEventListener('click', () => {
+    if (btnSinTextoTop.checked) {
+        topText.classList.add('none');
+    } else if (!btnSinTextoTop.checked) {
+        topText.classList.remove('none');
+    }
+});
+
+btnSinTextoBottom.addEventListener('click', () => {
+    if (btnSinTextoBottom.checked) {
+        bottomText.classList.add('none');
+    } else if (!btnSinTextoBottom.checked) {
+        bottomText.classList.remove('none');
+    }
+});
+
+//cambiar la fuente
